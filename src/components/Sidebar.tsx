29d813e85afe108eb2ffb4344c633c80348393d7
sparkie11 <gautamwise.s@gmail.com>
2025-06-22 14:50:58 +0530

    log out button added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,22 +1,22 @@
 // src/components/Sidebar.tsx
 import Image from 'next/image';
 import React from 'react';
-import { FaHome, FaPlus, FaList, FaShoppingCart, FaChartLine, FaTicketAlt, FaCog, FaArrowRight, } from 'react-icons/fa';
+import { FaHome, FaPlus, FaList, FaShoppingCart, FaChartLine, FaTicketAlt, FaCog, FaArrowRight, FaSignOutAlt } from 'react-icons/fa';
 
 const Sidebar: React.FC = () => {
   return (
-    <aside className="hidden sm:flex w-20 text-white flex-col items-center py-4 bg-custom-dark-blue">
+    <aside className="hidden sm:flex w-20 text-white flex-col items-center py-4 bg-custom-dark-blue" style={{backgroundColor:"#130562"}}>
       <div className="mb-8">
         <Image src="/logo-small.svg" height={100} width={100} alt="Logo" className="h-10 w-10" />
       </div>
-      <nav className="flex flex-col space-y-6">
+      <nav className="flex flex-col space-y-6 flex-grow">
         <a href="#" className="p-2 rounded-md hover:bg-gray-700">
-          <FaArrowRight size={24} />
+          <FaSignOutAlt size={24} />
         </a>
         <a href="#" className="p-2 rounded-md hover:bg-gray-700">
           <FaHome size={24} />
         </a>
-        <a href="#" className="p-2 rounded-md bg-blue-600">
+        <a href="#" className="p-2 rounded-md bg-custom-blue">
           <FaPlus size={24} />
         </a>
         <a href="#" className="p-2 rounded-md hover:bg-gray-700">
@@ -35,6 +35,12 @@ const Sidebar: React.FC = () => {
           <FaCog size={24} />
         </a>
       </nav>
+      <div>
+      <a href="#" className="p-2 rounded-md hover:bg-gray-700">
+          <FaSignOutAlt size={24} />
+        </a>
+
+      </div>
     </aside>
   );
 };
