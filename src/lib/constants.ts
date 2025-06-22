78bb38dda1e37da176122e5a3ce07e3b4f69164c
sparkie11 <gautamwise.s@gmail.com>
2025-06-22 14:57:38 +0530

    constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+import { FaHome, FaPlus, FaList, FaShoppingCart, FaChartLine, FaTicketAlt, FaCog, FaSignOutAlt } from 'react-icons/fa';
+
+export const navItems = [
+  { icon: FaHome, text: 'Home', href: '#' },
+  { icon: FaPlus, text: 'Add Inventory', href: '#' },
+  { icon: FaList, text: 'Inventory List', href: '#' },
+  { icon: FaShoppingCart, text: 'Orders', href: '#' },
+  { icon: FaChartLine, text: 'Analytics', href: '#' },
+  { icon: FaTicketAlt, text: 'Events', href: '#' },
+  { icon: FaCog, text: 'Settings', href: '#' },
+];
+
+export const logoutItem = { icon: FaSignOutAlt, text: 'Logout', href: '#' };
