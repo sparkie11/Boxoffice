420abe01ad2a39896e0ef681da1f43ee24cfa51e
sparkie11 <gautamwise.s@gmail.com>
2025-08-07 01:24:00 +0530

    overview and other changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  // ...other Next.js configurations
+  eslint: {
+    // This option allows production builds to successfully complete even if
+    // your project has ESLint errors. It's not recommended unless you already
+    // have ESLint configured to run in a separate part of your workflow (e.g., in CI).
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
